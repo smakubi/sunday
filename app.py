485574704a3cb7a2f1cca01fb35439b86169a97f
Samwel Emmanuel <samakubi@gmail.com>
2025-04-20 12:53:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 import streamlit as st
 
 st.title("My App")
-st.header("_Streamlit_ is :blue[cool] :sunglasses:")
-st.header("This is a header with a divider", divider="gray")
-st.header("These headers have rotating dividers", divider=True)
-st.header("One", divider=True)
-st.header("Two", divider=True)
-st.header("Three", divider=True)
-st.header("Four", divider=True)
 
 prompt = st.chat_input("Say something")
 if prompt:
