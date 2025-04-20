00fc5676caffa9d3c94d42065f1072bc993d12a3
Samwel Emmanuel <samakubi@gmail.com>
2025-04-20 12:57:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 st.title("Sunday App")
 
+simple_key = st.secrets("OPENAI_API_KEY")
+st.write(simple_key)
+
 prompt = st.chat_input("Say something")
 if prompt:
     st.write(f"User has sent the following prompt: {prompt}")
