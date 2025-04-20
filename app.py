fa5cad59e0dafa125c984a69f502f885e161a15a
Samwel Emmanuel <samakubi@gmail.com>
2025-04-20 12:59:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("Sunday App")
 
-simple_key = st.secrets("OPENAI_API_KEY")
+simple_key = st.secrets["OPENAI_API_KEY"]
 st.write(simple_key)
 
 prompt = st.chat_input("Say something")
